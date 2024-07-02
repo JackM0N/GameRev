420bd12bca871cc9ec7fa53a3df90418ebdce09b
PrzemyslawDev <przemyslawgotowala@gmail.com>
2024-07-02 15:37:55 +0200

    First commit

---
+++ Spring/src/main/java/pl/ttsw/GameRev/GameRevApplication.java
@@ -0,0 +1,13 @@
+package pl.ttsw.GameRev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameRevApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GameRevApplication.class, args);
+	}
+
+}
---
+++ Spring/src/test/java/pl/ttsw/GameRev/GameRevApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.ttsw.GameRev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameRevApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
