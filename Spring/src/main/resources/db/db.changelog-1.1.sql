e921dfc6d10db68ab97a03cea0d69840625a1439
Stanislaw <31638@uniwersytetkaliski.edu.pl>
2024-07-15 14:34:28 +0200

    Added triggers to update score fields

--- Spring/src/main/resources/db/db.changelog-1.1.sql
+++ Spring/src/main/resources/db/db.changelog-1.1.sql
@@ -28,3 +28,57 @@ SET users_score = (
     FROM user_review ur
     WHERE ur.game_id = g.game_id
 );
+
+
+--changeset Stanislaw:9 labels:triggers
+--trigger updating users_score in game on user_review CRUD changes
+CREATE OR REPLACE FUNCTION update_users_score()
+    RETURNS TRIGGER AS '
+    BEGIN
+        IF TG_OP = ''INSERT'' OR TG_OP = ''DELETE'' OR OLD.score IS DISTINCT FROM NEW.score THEN
+            UPDATE game g
+            SET users_score = COALESCE((SELECT AVG(ur.score)
+                                        FROM user_review ur
+                                        WHERE ur.game_id = OLD.game_id OR ur.game_id = NEW.game_id), 0)
+            WHERE g.game_id = OLD.game_id OR g.game_id = NEW.game_id;
+        END IF;
+
+        RETURN NEW;
+    END;
+' LANGUAGE plpgsql;
+
+CREATE TRIGGER trigger_update_users_score
+    AFTER INSERT OR UPDATE OR DELETE ON user_review
+    FOR EACH ROW
+EXECUTE FUNCTION update_users_score();
+
+
+--trigger updating positive_ and negative_rating in user_review on rating CRUD changes
+CREATE OR REPLACE FUNCTION update_user_review_ratings()
+    RETURNS TRIGGER AS '
+BEGIN
+    UPDATE user_review ur
+    SET
+        positive_rating = (
+            SELECT COUNT(*)
+            FROM rating r
+            WHERE r.user_review_id = ur.user_review_id
+              AND r.is_positive = true
+        ),
+        negative_rating = (
+            SELECT COUNT(*)
+            FROM rating r
+            WHERE r.user_review_id = ur.user_review_id
+              AND r.is_positive = false
+        )
+    WHERE ur.user_review_id = OLD.user_review_id OR ur.user_review_id = NEW.user_review_id;
+
+    RETURN NEW;
+END;
+' LANGUAGE plpgsql;
+
+CREATE TRIGGER trigger_update_user_review_ratings
+    AFTER INSERT OR UPDATE OR DELETE ON rating
+    FOR EACH ROW
+EXECUTE FUNCTION update_user_review_ratings();
+
