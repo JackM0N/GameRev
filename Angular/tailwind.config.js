d38d4acb7ab47324b4d98bbb180625bd5efb9c5e
arek <31643@uniwersytetkaliski.edu.pl>
2024-07-22 00:02:37 +0200

    semi responsive registration screen

--- Angular/tailwind.config.js
+++ Angular/tailwind.config.js
@@ -19,10 +19,12 @@ module.exports = {
         inputBackGround: '#e8f0fe',
         arcadeGradient1: '#1A1825',
         arcadeGradient2: '#182B38',
+        arcadeOrangeGradient2: '#4A201D',
       },
       backgroundImage: {
         'title-background': 'radial-gradient(ellipse at center, rgba(255, 255, 255, 0.3) 0%, rgba(255, 255, 255, 0) 60%)',
         'arcade-radial-bg': 'radial-gradient(circle, rgba(0,144,153,1) 30%, rgba(30,27,43,1) 80%)',
+        'arcade-radial-bg-orange': 'radial-gradient(circle, #FF3D00 30%, rgba(30,27,43,1) 80%)',
         'arcade-internal-edge': 'linear-gradient(to top left, rgba(0,0,0,0) 0%, rgba(0,0,0,0) calc(50% - 2px), rgba(71, 26, 80, 1) 50%, rgba(0,0,0,0) calc(50% + 2px), rgba(0,0,0,0) 100%), linear-gradient(to top right, rgba(0,0,0,0) 0%, rgba(0,0,0,0) calc(50% - 2px), rgba(71, 26, 80, 1) 50%, rgba(0,0,0,0) calc(50% + 2px), rgba(0,0,0,0) 100%);',
         'arcade-rainbow': 'linear-gradient(to top left, rgba(0,0,0,0) 0%, rgba(0,0,0,0) calc(38% - 3px), #3C003E 38%, #ED0280 39%, #ED0280 45%, black 46%, #00B2FF 47%, #00B2FF 53%, black 54%, #FF3D00 55%, #FF3D00 61%, #3C003E 62%, rgba(0,0,0,0) calc(62% + 3px), rgba(0,0,0,0) 100%);',
       },
@@ -31,6 +33,7 @@ module.exports = {
         'arcadeScreenShadow': '0px 0px 15px -1px rgba(0, 240, 255, 1)',
         'arcadeScreenShadowOrange': '0px 0px 15px -1px #FF3D00',
         'arcadeScreenInnerShadow': 'inset 0px 0px 15px -1px rgba(0, 240, 255, 1)',
+        'arcadeScreenInnerShadow': 'inset 0px 0px 15px -1px #FF3D00',
       },
       keyframes: {
         wiggle: {
