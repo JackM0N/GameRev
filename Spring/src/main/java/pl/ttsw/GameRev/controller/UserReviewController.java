035ecebe98daea3b464212c14ee3d64690a43532
PrzemyslawDev <przemyslawgotowala@gmail.com>
2024-07-23 13:20:54 +0200

    Added working endpoint for reporting userReview

--- Spring/src/main/java/pl/ttsw/GameRev/controller/UserReviewController.java
+++ Spring/src/main/java/pl/ttsw/GameRev/controller/UserReviewController.java
@@ -3,8 +3,10 @@ package pl.ttsw.GameRev.controller;
 import org.apache.coyote.BadRequestException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import pl.ttsw.GameRev.dto.ReportDTO;
 import pl.ttsw.GameRev.dto.UserReviewDTO;
 import pl.ttsw.GameRev.service.RatingService;
+import pl.ttsw.GameRev.service.ReportService;
 import pl.ttsw.GameRev.service.UserReviewService;
 import java.util.List;
 
@@ -13,10 +15,12 @@ import java.util.List;
 public class UserReviewController {
     private final UserReviewService userReviewService;
     private final RatingService ratingService;
+    private final ReportService reportService;
 
-    public UserReviewController(UserReviewService userReviewService, RatingService ratingService) {
+    public UserReviewController(UserReviewService userReviewService, RatingService ratingService, ReportService reportService) {
         this.userReviewService = userReviewService;
         this.ratingService = ratingService;
+        this.reportService = reportService;
     }
 
     @GetMapping("/id/{id}")
@@ -69,5 +73,13 @@ public class UserReviewController {
             return ResponseEntity.badRequest().body("There was an error rating the user review");
         }
         return ResponseEntity.ok(ratingService.updateRating(userReviewDTO));
+    }
+
+    @PutMapping("/report")
+    public ResponseEntity<?> reportUserReview(@RequestBody ReportDTO reportDTO) throws BadRequestException {
+        if (reportDTO == null){
+            return ResponseEntity.badRequest().body("There was an error when trying to report this users review");
+        }
+        return ResponseEntity.ok(reportService.createReport(reportDTO));
     }
 }
---
+++ Spring/src/main/java/pl/ttsw/GameRev/dto/ReportDTO.java
@@ -0,0 +1,22 @@
+package pl.ttsw.GameRev.dto;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.Value;
+
+import java.io.Serializable;
+
+/**
+ * DTO for {@link pl.ttsw.GameRev.model.Report}
+ */
+@Data
+@Getter
+@Setter
+public class ReportDTO implements Serializable {
+    Long id;
+    UserReviewDTO userReview;
+    WebsiteUserDTO user;
+    Boolean approved;
+    String content;
+}
