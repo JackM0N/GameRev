0fc20f49e99b2d73df94039390b65fc206df2ece
PrzemyslawDev <przemyslawgotowala@gmail.com>
2024-08-06 11:28:26 +0200

    Another mapper fix

--- Spring/src/main/java/pl/ttsw/GameRev/mapper/UserReviewMapper.java
+++ Spring/src/main/java/pl/ttsw/GameRev/mapper/UserReviewMapper.java
@@ -6,8 +6,12 @@ import pl.ttsw.GameRev.model.UserReview;
 
 @Mapper(unmappedTargetPolicy = ReportingPolicy.IGNORE, componentModel = MappingConstants.ComponentModel.SPRING)
 public interface UserReviewMapper {
+    @Mapping(source = "userUsername", target = "user.username")
+    @Mapping(source = "gameTitle", target = "game.title")
     UserReview toEntity(UserReviewDTO userReviewDTO);
 
+    @Mapping(source = "user.username", target = "userUsername")
+    @Mapping(source = "game.title", target = "gameTitle")
     UserReviewDTO toDto(UserReview userReview);
 
     @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
