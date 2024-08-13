6b108cc2b24e9879e1facb81ab83256327e8c920
PrzemyslawDev <przemyslawgotowala@gmail.com>
2024-08-13 12:39:06 +0200

    Added filtering to UserGame

--- Spring/src/main/java/pl/ttsw/GameRev/repository/UserGameRepository.java
+++ Spring/src/main/java/pl/ttsw/GameRev/repository/UserGameRepository.java
@@ -2,11 +2,13 @@ package pl.ttsw.GameRev.repository;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 import pl.ttsw.GameRev.model.UserGame;
 
 @Repository
-public interface UserGameRepository extends JpaRepository<UserGame, Long> {
+public interface UserGameRepository extends JpaRepository<UserGame, Long>, JpaSpecificationExecutor<UserGame> {
     Page<UserGame> findByUserNickname(String nickname, Pageable pageable);
 }
