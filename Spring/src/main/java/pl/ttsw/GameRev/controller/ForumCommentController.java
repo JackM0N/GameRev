fbdc32afd85e16188f03dc82e003f0734c19f3f1
PrzemyslawDev <przemyslawgotowala@gmail.com>
2024-08-20 13:42:09 +0200

    Added endpoints for CRUD operations on comments

---
+++ Spring/src/main/java/pl/ttsw/GameRev/controller/ForumCommentController.java
@@ -0,0 +1,57 @@
+package pl.ttsw.GameRev.controller;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import pl.ttsw.GameRev.dto.ForumCommentDTO;
+import pl.ttsw.GameRev.service.ForumCommentService;
+
+@RestController
+@RequestMapping("/post")
+public class ForumCommentController {
+    private final ForumCommentService forumCommentService;
+
+    public ForumCommentController(ForumCommentService forumCommentService) {
+        this.forumCommentService = forumCommentService;
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> findById(
+            @PathVariable Long id,
+            @RequestParam(value = "userId", required = false)  Long userId,
+            @RequestParam(value = "searchText", required = false)  String searchText,
+            Pageable pageable){
+        Page<ForumCommentDTO> forumCommentDTOS = forumCommentService.getForumCommentsByPost(id, userId, searchText, pageable);
+        if(forumCommentDTOS.isEmpty()){
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }
+        return ResponseEntity.ok(forumCommentDTOS);
+    }
+
+    @PostMapping("/create")
+    public ResponseEntity<?> create(@RequestBody ForumCommentDTO forumCommentDTO) {
+        if (forumCommentDTO == null){
+            return ResponseEntity.badRequest().body("There was an error creating this comment");
+        }
+        return ResponseEntity.ok(forumCommentService.createForumComment(forumCommentDTO));
+    }
+
+    @PutMapping("/edit/{id}")
+    public ResponseEntity<?> edit(@PathVariable Long id, @RequestBody ForumCommentDTO forumCommentDTO) {
+        if (forumCommentDTO == null){
+            return ResponseEntity.badRequest().body("There was an error editing this comment");
+        }
+        return ResponseEntity.ok(forumCommentService.updateForumComment(id, forumCommentDTO));
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<?> delete(@PathVariable Long id) {
+        boolean gotDeleted = forumCommentService.deleteForumComment(id);
+        if(!gotDeleted){
+            return ResponseEntity.badRequest().body("There was an error deleting this comment");
+        }
+        return ResponseEntity.ok().build();
+    }
+}
--- Spring/src/main/java/pl/ttsw/GameRev/service/ForumCommentService.java
+++ Spring/src/main/java/pl/ttsw/GameRev/service/ForumCommentService.java
@@ -1,18 +1,95 @@
 package pl.ttsw.GameRev.service;
 
+import jakarta.persistence.criteria.Join;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.stereotype.Service;
 import pl.ttsw.GameRev.dto.ForumCommentDTO;
+import pl.ttsw.GameRev.mapper.ForumCommentMapper;
+import pl.ttsw.GameRev.model.ForumComment;
+import pl.ttsw.GameRev.model.ForumPost;
+import pl.ttsw.GameRev.model.WebsiteUser;
 import pl.ttsw.GameRev.repository.ForumCommentRepository;
+import pl.ttsw.GameRev.repository.ForumPostRepository;
+import pl.ttsw.GameRev.repository.RoleRepository;
+
+import java.time.LocalDate;
 
 @Service
 public class ForumCommentService {
     private final ForumCommentRepository forumCommentRepository;
+    private final ForumPostRepository forumPostRepository;
+    private final RoleRepository roleRepository;
+    private final ForumCommentMapper forumCommentMapper;
+    private final WebsiteUserService websiteUserService;
 
-    public ForumCommentService(ForumCommentRepository forumCommentRepository) {
+    public ForumCommentService(ForumCommentRepository forumCommentRepository, ForumPostRepository forumPostRepository, RoleRepository roleRepository, ForumCommentMapper forumCommentMapper, WebsiteUserService websiteUserService) {
         this.forumCommentRepository = forumCommentRepository;
+        this.forumPostRepository = forumPostRepository;
+        this.roleRepository = roleRepository;
+        this.forumCommentMapper = forumCommentMapper;
+        this.websiteUserService = websiteUserService;
     }
 
-    public ForumCommentDTO getForumCommentsByPost(Long id) {
-        return null;
+    public Page<ForumCommentDTO> getForumCommentsByPost(Long id, Long userId, String searchText, Pageable pageable) {
+        ForumPost post = forumPostRepository.findById(id).orElse(null);
+        if (post == null) {
+            return null;
+        }
+        Specification<ForumComment> spec = ((root, query, builder) -> builder.equal(root.get("post"), post));
+
+        if (userId != null) {
+            spec = spec.and((root, query, builder) -> {
+                Join<ForumComment, WebsiteUser> join = root.join("author");
+                return builder.equal(join.get("id"), userId);
+            });
+        }
+
+        if (searchText != null) {
+            searchText = searchText.toLowerCase();
+            String likePattern = "%" + searchText + "%";
+            spec = spec.and((root, query, builder) -> builder.like(builder.lower(root.get("content")), likePattern));
+        }
+
+        Page<ForumComment> forumComments = forumCommentRepository.findAll(spec, pageable);
+        return forumComments.map(forumCommentMapper::toDto);
+    }
+
+    public ForumCommentDTO createForumComment(ForumCommentDTO forumCommentDTO) {
+        ForumComment forumComment = new ForumComment();
+        forumComment.setForumPost(forumPostRepository.findById(forumCommentDTO.getForumPost().getId())
+                .orElseThrow(() -> new RuntimeException("Forum post not found")));
+        forumComment.setAuthor(websiteUserService.getCurrentUser());
+        forumComment.setContent(forumCommentDTO.getContent());
+        forumComment.setPostDate(LocalDate.now());
+        forumCommentRepository.save(forumComment);
+        return forumCommentMapper.toDto(forumComment);
+    }
+
+    public ForumCommentDTO updateForumComment(Long id, ForumCommentDTO forumCommentDTO) {
+        WebsiteUser currentUser = websiteUserService.getCurrentUser();
+        ForumComment forumComment = forumCommentRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Forum comment not found"));
+        forumComment.setContent(forumCommentDTO.getContent());
+        if (currentUser.getRoles().contains(roleRepository.findByRoleName("Admin").get()) || currentUser == forumComment.getAuthor()) {
+            forumCommentRepository.save(forumComment);
+            return forumCommentMapper.toDto(forumComment);
+        }else {
+            throw new BadCredentialsException("You dont have permission to perform this action");
+        }
+    }
+
+    public boolean deleteForumComment(Long id) {
+        WebsiteUser currentUser = websiteUserService.getCurrentUser();
+        ForumComment forumComment = forumCommentRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Forum comment not found"));
+        if (currentUser.getRoles().contains(roleRepository.findByRoleName("Admin").get()) || currentUser == forumComment.getAuthor()) {
+            forumCommentRepository.delete(forumComment);
+            return true;
+        }else {
+            throw new BadCredentialsException("You dont have permission to perform this action");
+        }
     }
 }
