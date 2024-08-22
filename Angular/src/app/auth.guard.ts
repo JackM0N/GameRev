46ca9c32dd4eb93e4f72d9607c1a80f3e6a3d83f
Maja552 <mikazukikanade@gmail.com>
2024-08-22 13:23:13 +0200

    Fixed auth guard

--- Angular/src/app/auth.guard.ts
+++ Angular/src/app/auth.guard.ts
@@ -1,8 +1,5 @@
 import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, CanActivate, Router } from '@angular/router';
-import { isPlatformBrowser } from '@angular/common';
-import { PLATFORM_ID } from '@angular/core';
-import { Inject } from '@angular/core';
 import { AuthService } from './services/auth.service';
 
 @Injectable({
@@ -11,15 +8,14 @@ import { AuthService } from './services/auth.service';
 export class AuthGuard implements CanActivate {
   constructor(
     private authService: AuthService,
-    private router: Router,
-    @Inject(PLATFORM_ID) private platformId: Object
+    private router: Router
   ) {}
 
   canActivate(route: ActivatedRouteSnapshot): boolean {
     const allowedRoles = route.data['roles'] as Array<string>;
     
     if (this.authService.isAuthenticated()) {
-      if (allowedRoles && this.authService.hasAnyRole(allowedRoles)) {
+      if (allowedRoles && !this.authService.hasAnyRole(allowedRoles)) {
         return false;
       }
       return true;
