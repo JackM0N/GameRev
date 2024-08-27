c95145046c359d4fa742fc81c0ae735a02d97adf
PrzemyslawDev <przemyslawgotowala@gmail.com>
2024-08-27 12:31:13 +0200

    Initial refactoring of Game based on internship supervisor's comments

--- Spring/src/main/java/pl/ttsw/GameRev/controller/GameController.java
+++ Spring/src/main/java/pl/ttsw/GameRev/controller/GameController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import pl.ttsw.GameRev.dto.GameDTO;
 import pl.ttsw.GameRev.enums.ReleaseStatus;
+import pl.ttsw.GameRev.filter.GameFilter;
 import pl.ttsw.GameRev.service.GameService;
 import java.io.IOException;
 import java.time.LocalDate;
@@ -25,32 +26,33 @@ import java.util.List;
 public class GameController {
     private final GameService gameService;
 
+    @GetMapping
+    public ResponseEntity<Page<GameDTO>> getAllGames(GameFilter gameFilter, Pageable pageable) {
+        return ResponseEntity.ok(gameService.getAllGames(gameFilter, pageable));
+    }
+
+    @GetMapping("/{title}")
+    public ResponseEntity<GameDTO> getGame(@PathVariable String title) {
+        title = title.replaceAll("-"," ");
+        return ResponseEntity.ok(gameService.getGameByTitle(title));
+    }
+
     @PostMapping("/create")
-    public ResponseEntity<?> createGame(@RequestParam(value = "picture", required = false) MultipartFile picture,
-                                        @RequestParam("game") String gameJson) throws IOException {
+    public ResponseEntity<GameDTO> createGame(@RequestParam(value = "picture", required = false) MultipartFile picture,
+                                              @RequestParam("game") String gameJson) throws IOException {
         ObjectMapper objectMapper = JsonMapper.builder()
                 .addModule(new JavaTimeModule())
                 .build();
         GameDTO request = objectMapper.readValue(gameJson, GameDTO.class);
         GameDTO game = gameService.createGame(request, picture);
         if (game == null) {
-            return ResponseEntity.badRequest().body("Game creation failed");
+            return ResponseEntity.badRequest().build();
         }
         return ResponseEntity.ok(game);
     }
 
-    @GetMapping("/{title}")
-    public ResponseEntity<?> getGame(@PathVariable String title) throws BadRequestException {
-        title = title.replaceAll("-"," ");
-        GameDTO game = gameService.getGameByTitle(title);
-        if (game == null) {
-            return ResponseEntity.notFound().build();
-        }
-        return ResponseEntity.ok(gameService.getGameByTitle(title));
-    }
-
     @PutMapping("/edit/{title}")
-    public ResponseEntity<?> editGame(@RequestParam(value = "picture", required = false) MultipartFile picture,
+    public ResponseEntity<GameDTO> editGame(@RequestParam(value = "picture", required = false) MultipartFile picture,
                                       @PathVariable String title,
                                       @RequestParam("game") String gameJson) throws IOException {
         title = title.replaceAll("-"," ");
@@ -59,9 +61,6 @@ public class GameController {
                 .build();
         GameDTO request = objectMapper.readValue(gameJson, GameDTO.class);
         GameDTO updatedGame = gameService.updateGame(title, request, picture);
-        if (updatedGame == null) {
-            return ResponseEntity.notFound().build();
-        }
         return ResponseEntity.ok(updatedGame);
     }
 
@@ -72,22 +71,5 @@ public class GameController {
             return ResponseEntity.notFound().build();
         }
         return ResponseEntity.noContent().build();
-    }
-
-    @GetMapping
-    public ResponseEntity<?> getAllGames(
-            @RequestParam(value = "fromDate", required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate fromDate,
-            @RequestParam(value = "toDate", required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate toDate,
-            @RequestParam(value = "minUserScore", required = false) Float minUserScore,
-            @RequestParam(value = "maxUserScore", required = false) Float maxUserScore,
-            @RequestParam(value = "tagIds", required = false) List<Long> tagIds,
-            @RequestParam(value = "releaseStatuses", required = false) List<ReleaseStatus> releaseStatuses,
-            Pageable pageable
-    ) {
-        Page<GameDTO> games = gameService.getAllGames(fromDate, toDate, minUserScore, maxUserScore, tagIds, releaseStatuses, pageable);
-        if (games.getTotalElements() == 0) {
-            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
-        }
-        return ResponseEntity.ok(games);
     }
 }
