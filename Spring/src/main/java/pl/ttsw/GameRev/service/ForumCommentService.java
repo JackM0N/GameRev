c0925b2c6e4048c201dd1513de03a494bb775a3c
Stanislaw <31638@uniwersytetkaliski.edu.pl>
2024-08-29 10:27:03 +0200

    Revert "Merge pull request #153 from JackM0N/przemyslaw-dev"
    This reverts commit 68360d1f15dfc4f3a5515109c09b30c0321c5579, reversing
    changes made to f934e49f1f38b5057d26452a58d33ec2f9149f8b.

--- Spring/src/main/java/pl/ttsw/GameRev/service/ForumCommentService.java
+++ Spring/src/main/java/pl/ttsw/GameRev/service/ForumCommentService.java
@@ -46,8 +46,12 @@ public class ForumCommentService {
     }
 
     public ForumCommentDTO createForumComment(ForumCommentDTO forumCommentDTO) {
-        ForumComment forumComment = forumCommentMapper.toEntity(forumCommentDTO);
+        ForumComment forumComment = new ForumComment();
+        forumComment.setForumPost(forumPostRepository.findById(forumCommentDTO.getForumPostId())
+                .orElseThrow(() -> new RuntimeException("Forum post not found")));
         forumComment.setAuthor(websiteUserService.getCurrentUser());
+        forumComment.setContent(forumCommentDTO.getContent());
+        forumComment.setPostDate(LocalDateTime.now());
         forumCommentRepository.save(forumComment);
         return forumCommentMapper.toDto(forumComment);
     }
@@ -56,9 +60,7 @@ public class ForumCommentService {
         WebsiteUser currentUser = websiteUserService.getCurrentUser();
         ForumComment forumComment = forumCommentRepository.findById(id)
                 .orElseThrow(() -> new RuntimeException("Forum comment not found"));
-
-        forumCommentMapper.partialUpdateContent(forumCommentDTO, forumComment);
-
+        forumComment.setContent(forumCommentDTO.getContent());
         if (currentUser.getRoles().stream().anyMatch(role -> "Admin".equals(role.getRoleName()))
                 || currentUser == forumComment.getAuthor()) {
             forumCommentRepository.save(forumComment);
