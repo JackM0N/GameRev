c51c54484329ce72d05219db350c13de820f69e9
PrzemyslawDev <przemyslawgotowala@gmail.com>
2024-08-30 12:55:15 +0200

    Added picture handling to comments

--- Spring/src/main/java/pl/ttsw/GameRev/controller/ForumCommentController.java
+++ Spring/src/main/java/pl/ttsw/GameRev/controller/ForumCommentController.java
@@ -1,15 +1,22 @@
 package pl.ttsw.GameRev.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.json.JsonMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 import pl.ttsw.GameRev.dto.ForumCommentDTO;
 import pl.ttsw.GameRev.dto.ForumPostDTO;
 import pl.ttsw.GameRev.filter.ForumCommentFilter;
 import pl.ttsw.GameRev.service.ForumCommentService;
+
+import java.io.IOException;
 
 @RestController
 @RequestMapping("/post")
@@ -30,20 +37,43 @@ public class ForumCommentController {
         return ResponseEntity.ok(forumCommentService.getForumCommentsByPost(id, forumCommentFilter, pageable));
     }
 
+    @GetMapping("/picture/{id}")
+    public ResponseEntity<byte[]> getPicture(@PathVariable Long id) {
+        try {
+            byte[] image = forumCommentService.getCommentPicture(id);
+            return ResponseEntity.ok()
+                    .contentType(MediaType.IMAGE_JPEG)
+                    .body(image);
+        } catch (IOException e) {
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }
+    }
+
     @PostMapping("/create")
-    public ResponseEntity<ForumCommentDTO> create(@RequestBody ForumCommentDTO forumCommentDTO) {
-        if (forumCommentDTO == null){
+    public ResponseEntity<ForumCommentDTO> create(
+            @RequestParam(value = "comment") String commentJson,
+            @RequestParam(value = "picture", required = false) MultipartFile picture) throws IOException {
+        ObjectMapper mapper = JsonMapper.builder()
+                .addModule(new JavaTimeModule())
+                .build();
+        ForumCommentDTO request = mapper.readValue(commentJson, ForumCommentDTO.class);
+        ForumCommentDTO comment = forumCommentService.createForumComment(request, picture);
+        if (comment == null){
             return ResponseEntity.badRequest().build();
         }
-        return ResponseEntity.ok(forumCommentService.createForumComment(forumCommentDTO));
+        return ResponseEntity.ok(comment);
     }
 
     @PutMapping("/edit/{id}")
-    public ResponseEntity<ForumCommentDTO> edit(@PathVariable Long id, @RequestBody ForumCommentDTO forumCommentDTO) {
-        if (forumCommentDTO == null){
-            return ResponseEntity.badRequest().build();
-        }
-        return ResponseEntity.ok(forumCommentService.updateForumComment(id, forumCommentDTO));
+    public ResponseEntity<ForumCommentDTO> edit(
+            @PathVariable Long id,
+            @RequestParam(value = "comment") String commentJson,
+            @RequestParam(value = "picture") MultipartFile picture) throws IOException {
+        ObjectMapper objectMapper = JsonMapper.builder()
+                .addModule(new JavaTimeModule())
+                .build();
+        ForumCommentDTO request = objectMapper.readValue(commentJson, ForumCommentDTO.class);
+        return ResponseEntity.ok(forumCommentService.updateForumComment(id, request, picture));
     }
 
     @DeleteMapping("/delete/{id}")
