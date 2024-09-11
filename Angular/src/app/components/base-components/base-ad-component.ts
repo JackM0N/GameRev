932f95d74b30cf8df2202469295a0f46e7ee4908
Maja552 <mikazukikanade@gmail.com>
2024-09-11 10:23:41 +0200

    Made it so ads stay in place when dialog windows are open

--- Angular/src/app/components/base-components/base-ad-component.ts
+++ Angular/src/app/components/base-components/base-ad-component.ts
@@ -1,4 +1,4 @@
-﻿import { AfterViewInit, ChangeDetectorRef, Component, SimpleChanges } from '@angular/core';
+﻿import { AfterViewInit, ChangeDetectorRef, Component } from '@angular/core';
 import { BackgroundService } from '../../services/background.service';
 import { AdService } from '../../services/ad.service';
 
