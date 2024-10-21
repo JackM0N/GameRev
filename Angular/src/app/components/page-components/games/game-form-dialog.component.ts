6bda689fd476c96489922634af0a4ba68664331e
Maja552 <mikazukikanade@gmail.com>
2024-10-21 14:52:40 +0200

    Improved game form dialog

--- Angular/src/app/components/page-components/games/game-form-dialog.component.ts
+++ Angular/src/app/components/page-components/games/game-form-dialog.component.ts
@@ -19,17 +19,6 @@ export class GameFormDialogComponent {
   protected releaseStatuses: ReleaseStatus[] = releaseStatuses;
   protected listTitle: string = 'Add a new game';
   protected tagsList: Tag[] = [];
-  private gameDate: Date = new Date();
-  
-  private game: Game = {
-    title: '',
-    developer: '',
-    publisher: '',
-    releaseDate: [],
-    description: '',
-    tags: [],
-    usersScore: 0
-  };
 
   constructor(
     private formBuilder: FormBuilder,
@@ -43,15 +32,21 @@ export class GameFormDialogComponent {
     }
   ) {
     this.addingGameForm = this.formBuilder.group({
-      title: [this.game.title, [Validators.required, Validators.minLength(1)]],
-      developer: [this.game.developer, [Validators.required, Validators.minLength(1)]],
-      publisher: [this.game.publisher, [Validators.required, Validators.minLength(1)]],
-      releaseDate: [this.game.releaseDate],
-      releaseStatus: [this.game.releaseStatus, [Validators.required, Validators.minLength(1)]],
-      tags: [this.game.tags],
-      description: [this.game.description, [Validators.required, Validators.minLength(1)]],
-      usersScore: [this.game.usersScore]
+      title: [undefined, [Validators.required, Validators.minLength(1)]],
+      developer: [undefined, [Validators.required, Validators.minLength(1)]],
+      publisher: [undefined, [Validators.required, Validators.minLength(1)]],
+      releaseDate: [undefined],
+      releaseStatus: [undefined, [Validators.required, Validators.minLength(1)]],
+      tags: [undefined],
+      description: [undefined, [Validators.required, Validators.minLength(1)]],
+      usersScore: [undefined]
     });
+
+    if (this.data.editing) {
+      this.listTitle = 'Editing game';
+    }
+
+    this.loadTags();
   }
 
   onCancel(): void {
@@ -59,30 +54,13 @@ export class GameFormDialogComponent {
   }
   
   ngOnInit() {
-    this.loadTags();
-
-    if (this.data.editing) {
-      this.listTitle = 'Editing game';
-    }
-
     if (this.data.game) {
-      this.game = this.data.game;
-
-      if (this.game.releaseDate) {
-        this.gameDate = new Date(this.game.releaseDate[0], this.game.releaseDate[1] -1, this.game.releaseDate[2], 15);
-      }
-
       this.addingGameForm.patchValue({
-        title: this.game.title,
-        developer: this.game.developer,
-        publisher: this.game.publisher,
-        releaseDate: this.gameDate,
-        releaseStatus: this.game.releaseStatus,
-        description: this.game.description,
-        usersScore: this.game.usersScore
+        ...this.data.game,
+        releaseDate: new Date(this.data.game.releaseDate[0], this.data.game.releaseDate[1] -1, this.data.game.releaseDate[2], 15)
       });
 
-      var status = this.releaseStatuses.find(rs => rs.name === this.game.releaseStatus);
+      var status = this.releaseStatuses.find(rs => rs.className === this.data.game.releaseStatus);
 
       if (status) {
         this.addingGameForm.patchValue({
@@ -93,9 +71,8 @@ export class GameFormDialogComponent {
   }
 
   onSubmit() {
-    if (this.addingGameForm.valid) {
+    if (this.addingGameForm.valid && this.data.game.title) {
       const gameData = {
-        ...this.game,
         ...this.addingGameForm.value
       };
 
@@ -103,13 +80,11 @@ export class GameFormDialogComponent {
       gameData.releaseStatus = gameData.releaseStatus.className;
 
       if (this.data.editing) {
-        if (this.game.title) {
-          this.gameService.editGame(this.game.title, gameData).subscribe({
-            next: () => { this.notificationService.popSuccessToast('Edited game successfuly'); },
-            error: error => this.notificationService.popErrorToast('Editing game failed', error)
-          });
-          this.dialogRef.close(true);
-        }
+        this.gameService.editGame(this.data.game.title, gameData).subscribe({
+          next: () => { this.notificationService.popSuccessToast('Edited game successfuly'); },
+          error: error => this.notificationService.popErrorToast('Editing game failed', error)
+        });
+        this.dialogRef.close(true);
         return;
       }
 
@@ -128,17 +103,17 @@ export class GameFormDialogComponent {
           this.tagsList = response;
           this.tagsList.sort((a, b) => b.priority - a.priority);
   
-          var tags = [];
-          if (this.game.tags) {
-            for (let i = 0; i < this.tagsList.length; i++) {
-              for (let j = 0; j < this.game.tags.length; j++) {
-                if (this.tagsList[i].id == this.game.tags[j].id) {
-                  tags.push(this.tagsList[i]);
-                }
+          const tags: any = [];
+          const formTags = this.addingGameForm.get('tags');
+          if (formTags) {
+            formTags.value.forEach((tag: any) => {
+              const matchingTag = this.tagsList.find((t: any) => t.id === tag.id);
+              if (matchingTag) {
+                tags.push(matchingTag);
               }
-            }
+            });
           }
-
+          
           this.addingGameForm.patchValue({
             tags: tags,
           });
